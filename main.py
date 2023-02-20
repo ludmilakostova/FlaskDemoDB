f8495ee857719a544b1f0f90a86a5f6e4d285e35
Ludmila Kostova <ludmila.kostova@yahoo.fr>
2023-02-20 16:49:51 +1100

    Finalized example

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
+from decouple import config
 from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Resource, Api
 from flask_migrate import Migrate
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = f'postgresql://postgres:sevillaespagne@localhost:5432/store'
+app.config["SQLALCHEMY_DATABASE_URI"] = f"postgresql://{config('DB_USER')}:{config('DB_PASSWORD')}\
+@localhost:{config('DB_PORT')}/{config('DB_NAME')}"
 
 db = SQLAlchemy(app)
 api = Api(app)
@@ -18,15 +20,27 @@ class BookModel(db.Model):
     title = db.Column(db.String, nullable=False)
     author = db.Column(db.String, nullable=False)
     test_something = db.Column(db.String)
+    reader_pk = db.Column(db.Integer, db.ForeignKey('readers.pk'))
+    reader = db.relationship('ReaderModel')
 
     def as_dict(self):
         return {c.name: getattr(self, c.name) for c in self.__table__.columns}
 
 
+class ReaderModel(db.Model):
+    __tablename__ = 'readers'
+    pk = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String, nullable=False)
+    last_name = db.Column(db.String, nullable=False)
+    books = db.relationship("BookModel", backref="book", lazy='dynamic')
+
+
 class BookResource(Resource):
     def post(self):
         data = request.get_json()
+        reader_pk = data.pop("reader_pk")
         book: BookModel = BookModel(**data)
+        book.reader_pk = reader_pk
         db.session.add(book)
         db.session.commit()
         return book.as_dict()
